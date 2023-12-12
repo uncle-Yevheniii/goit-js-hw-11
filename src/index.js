01fe3b214f58295f167c96cc6c2c67fe5646217f
uncle-Jonik <Katsimon_Eu@outlook.com>
2023-12-12 20:31:19 +0200

    v0.1

--- src/index.js
+++ src/index.js
@@ -2,54 +2,67 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { createMarkup } from './markup';
 
+const form = document.querySelector('.search-form');
+const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more-hidden');
+let page = 1;
 
+//
 
-const allReferences = {
-    form: document.querySelector('.search-form'),
-    gallery: document.querySelector('.gallery')
-} 
+form.addEventListener('submit', onValueSubmit);
 
+function onValueSubmit(event) {
+  event.preventDefault();
 
-const allParameters = {
-    BASE_URL: 'https://pixabay.com/api/',
-    API_KEY: '41201179-9e6e53d17bde192e39b3718f4',
-    q: '',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch : true,
-    
-}
-allReferences.form.addEventListener('submit', onValueSubmit)
-
-
-function onValueSubmit (event) {
-    event.preventDefault();
-
-
-    const searchValue = event.currentTarget[0].value;
-    const value = formattedValue(searchValue)
+  enteredValue = event.currentTarget[0].value;
+  render(enteredValue);
+  form.reset();
 }
 
+//
+// Запит на сервер
+async function getGallery(page = 1, qValue) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '41201179-9e6e53d17bde192e39b3718f4';
+  const q = qValue;
+  const image_type = 'photo';
+  const orientation = 'horizontal';
+  const safesearch = 'true';
+  const per_page = 4;
 
-
-
-// форматування
-function formattedValue (str) {
-    const splitString = str.split(' ');
-    const joinString = splitString.join('+')
-    return joinString
+  const queryParams = new URLSearchParams({
+    key: API_KEY,
+    q,
+    image_type,
+    page,
+    per_page,
+    orientation,
+    safesearch,
+  });
+  try {
+    const res = await axios.get(`${BASE_URL}?${queryParams}`);
+    return await res.data;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
+//
+//
+async function render(qValue) {
+  try {
+    const data = await getGallery(page, qValue);
 
-
-async function getGallery (value) {
-    const response = await axios(allParameters.BASE_URL, {
-        params: {
-            api_key: '41201179-9e6e53d17bde192e39b3718f4',
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch : true,
-            q: value,
-        }
-    })
+    if (!data.hits.length > 0) {
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    console.log(data);
+    gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+  } catch (error) {
+    console.log('error!', error);
+  }
 }
---
+++ src/markup.js
@@ -0,0 +1,38 @@
+// розмітка картки зображення
+export function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+        <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            ${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            ${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            ${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            ${downloads}
+          </p>
+        </div>
+      </div>
+    `
+    )
+    .join('');
+}
